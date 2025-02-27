fecb024ea64d5a03fada66e64b531d5594dc5a00
AbdullahEltony <abdullahmeltony@gmail.com>
2025-02-27 20:32:25 +0200

    Modified password validation

--- src/lib/validation.ts
+++ src/lib/validation.ts
@@ -19,5 +19,8 @@ export const RegisterSchema = yup.object().shape({
     .string()
     .required("Email is required")
     .email("Email must be a valid email address"),
-  password: yup.string().required("Password is required"),
+  password: yup
+    .string()
+    .required("Password is required")
+    .min(6, "Password must be at least 6 characters long"),
 });
