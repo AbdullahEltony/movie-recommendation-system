39741a4c9305bccd42ecd1b4a839e03800a2d813
AbdullahEltony <abdullahmeltony@gmail.com>
2025-03-02 21:44:18 +0200

    Modified section title

--- src/components/home/Recommended.tsx
+++ src/components/home/Recommended.tsx
@@ -6,7 +6,7 @@ import { recommendedMoviesData } from "@/lib/placeholders";
 const Recommended = () => {
   return (
     <div className="mx-[2rem] sm:mx-0 mt-48 sm:mt-32">
-      <SectionTitle title="Recommended For Your" />
+      <SectionTitle title="Recommended For You" />
       <SliderWrapper>
         <MovieSlider sliderType="recommended" movieList={recommendedMoviesData}/>
       </SliderWrapper>
